f43ea980ffab7cf4fa9152bbbba0e512689285fd
shandrolis <joepvdstaaij@live.nl>
2015-12-02 21:09:55 +0100

    removed duplicate

--- Week2.4_2/src/Ruuds_Vervoers_Bedrijf/Personenvervoer/Trein.java
+++
@@ -1,7 +0,0 @@
-package Ruuds_Vervoers_Bedrijf.Personenvervoer;
-
-/**
- * Created by Joep on 02-Dec-15.
- */
-public class Trein {
-}
--- Week2.4_2/src/Ruuds_Vervoers_Bedrijf/Vrachtvervoer/Vrachtwagen.java
+++
@@ -1,7 +0,0 @@
-package Ruuds_Vervoers_Bedrijf.Vrachtvervoer;
-
-/**
- * Created by Joep on 02-Dec-15.
- */
-public class Vrachtwagen {
-}
