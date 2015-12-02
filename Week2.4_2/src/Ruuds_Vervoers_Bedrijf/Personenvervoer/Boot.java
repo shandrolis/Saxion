3966eb4e0dc4748d14950f3252b807dc2baaa8e8
shandrolis <joepvdstaaij@live.nl>
2015-12-02 20:41:05 +0100

    Project structure

---
+++ Week2.4_2/src/Ruuds_Vervoers_Bedrijf/Personenvervoer/Boot.java
@@ -0,0 +1,7 @@
+package Ruuds_Vervoers_Bedrijf.Personenvervoer;
+
+/**
+ * Created by Joep on 02-Dec-15.
+ */
+public class Boot {
+}
---
+++ Week2.4_2/src/Ruuds_Vervoers_Bedrijf/Vrachtvervoer/Vrachtwagen.java
@@ -0,0 +1,7 @@
+package Ruuds_Vervoers_Bedrijf.Vrachtvervoer;
+
+/**
+ * Created by Joep on 02-Dec-15.
+ */
+public class Vrachtwagen {
+}
