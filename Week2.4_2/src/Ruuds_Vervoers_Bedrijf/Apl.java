1ff8761b90424a92b7d77e5d78aeb2ef607f1687
shandrolis <joepvdstaaij@live.nl>
2015-12-02 20:35:58 +0100

    testing Intellij built-in commit tool

---
+++ Week2.4_2/src/Ruuds_Vervoers_Bedrijf/Apl.java
@@ -0,0 +1,17 @@
+package Ruuds_Vervoers_Bedrijf;
+
+public class Apl {
+
+    public static void main(String[] args) {
+
+        new Apl().run();;
+
+    }
+
+
+
+
+    public void run(){
+
+    }
+}
